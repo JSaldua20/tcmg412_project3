2dd75f6c1decd246b5322d0a523dc4b7b3e3e09c
tazmaniandevil64 <44484776+tazmaniandevil64@users.noreply.github.com>
2021-02-22 18:43:08 -0600

    Begin the if else statement
    starting the if else statement to check if the file already exists or not, and if it doesn't, download it from the URL

--- marketing_analytics.py
+++ marketing_analytics.py
@@ -3,3 +3,9 @@ import urllib.request
 
 FilePath = './http_access_log.txt'
 FileExists = os.path.exists(FilePath)
+
+if FileExists == False:
+  print("You don't seem to have the data, downloading it now")
+  url = "https://s3.amazonaws.com/tcmg476/http_access_log"
+  urllib.request.urlretrieve(url, FilePath)
+  
